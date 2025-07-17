d3426964c39ca507b9ae410479b3ee41b1830c19
tovawieds <wiedermanntova@gmail.com>
2025-07-16 22:00:18 -0400

    Add CORS

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
+from flask_cors import CORS
 import db
 
 app = Flask(__name__)
+cors = CORS(app, resources={"/*": {"origins": "http://localhost:5173", "supports_credentials": True}})
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def Welcome():
+    return 'Welcome to the To Do App!'
 
 # index route
 @app.route("/todos.json")
